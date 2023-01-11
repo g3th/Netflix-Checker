fc535dda7ee2b12a9d23de3ce34f4b5cdb151d74
g3th <theinkandpen@yahoo.co.uk>
2023-01-11 00:04:52 +0000

    new file:   test/test_splitting.py

---
+++ test/test_splitting.py
@@ -0,0 +1,11 @@
+email = []
+password = []
+
+with open('netflix','r') as net:
+	for line in net.readlines():
+		email.append(line.split("@")[0]+line.split(":")[0])
+		password.append(line.split(":")[1].split(" ")[0])
+		
+
+
+print(email[1], password[1])
