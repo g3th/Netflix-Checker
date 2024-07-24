a81e07349520ca558f8532298bbbdf505c6ea26f
roberto <74467654+g3th@users.noreply.github.com>
2024-07-24 22:15:28 +0100

    removed: bloat, extra options in TUI

--- netflix.py
+++ netflix.py
@@ -3,8 +3,6 @@ import time
 import requests
 import os
 import sys
-from split_method import determine_split_method
-from split_method import return_error
 from pathlib import Path
 from header import title
 from header import user_options
@@ -16,8 +14,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 
 details = [find_IP()[0], find_IP()[1], find_IP()[2]]
-current_split_method = determine_split_method()
-list_without_countries = False
 counter = 0
 hits = 0
 user = []
@@ -28,90 +24,24 @@ resume_flag = False
 clear_page = 0
 directory = str(Path(__file__).parent)
 
-
-def print_ip_and_country():
-    blacklisted_split_methods = ['None', ';', ' |', '[NETFLIX]']
-    split_method_has_a_country = False
-    if [item for item in blacklisted_split_methods if (current_split_method != item)]:
-        if isinstance(details[2], list):
-            print("\n\033[38;5;7mCurrent IP: {} : {} | Fetching {}/{} accounts\n".format(details[0], details[1],
-                                                                                         *details[2]))
-        else:
-            if list_without_countries == True:
-                print("\n\033[38;5;7mCurrent IP: {} : {} | Fetching All Accounts\n".format(details[0], details[1]))
-            else:
-                print("\n\033[38;5;7mCurrent IP: {} : {} | Fetching {} accounts\n".format(details[0], details[1],
-                                                                                          details[2]))
-        split_method_has_a_country = True
-    else:
-        print("\n\033[38;5;7mCurrent IP: {} : {} | No Countries in Combo\n".format(details[0], details[1]))
-    return split_method_has_a_country
-
-
 for file_ in os.listdir(directory):
     files.append(file_)
-
-for file_ in range(len(files)):
-    if 'resume' in files[file_]:
-        resume_flag = True
-        with open('resume', 'r') as resume:
-            for line in resume.readlines():
-                user.append(line.split(":")[0])
-                passw.append(line.split(":")[1])
-        break
-    if file_ == len(files) - 1:
-        with open('netflix', 'r') as net:
-            for line in net.readlines():
-                if isinstance(details[2], list) and current_split_method != "None":
-                    if [country for country in details[2] if (country in line)]:
-                        user.append(line.split(":")[0])
-                        passw.append(line.split(":")[1].split(current_split_method)[0].strip())
-
-                if isinstance(details[2], list) == False:
-                    if details[2] in line and current_split_method != ";":
-                        user.append(line.split(":")[0])
-                        passw.append(line.split(":")[1].split(current_split_method)[0].strip())
-
-                    elif details[2] in line and current_split_method != ";":
-                        user.append(line.split(":")[0])
-                        passw.append(line.split(":")[1].split(current_split_method)[0].strip())
-
-                if current_split_method == ";":
-                    user.append(line.split(";")[1].split(";")[0])
-                    passw.append(line.split(";")[2].strip())
-
-                if current_split_method == "[NETFLIX]":
-                    user.append(line.split(" - ")[1].split(":")[0].strip())
-                    passw.append(line.split(" - ")[1].split(":")[1].strip())
-
-                if current_split_method == "None":
-                    user.append(line.split(":")[0])
-                    passw.append(line.split(":")[1])
-
-                if current_split_method == "https":
-                    user.append(line.split(" | ")[1].split(":")[0])
-                    passw.append(line.split(" | ")[1].split(":")[1])
-
-            for line in range(len(user)):
-                updated_list.append("{}:{}\n".format(user[line], passw[line]))
-        net.close()
 
 page = "https://www.netflix.com/login"
 while True:
     logging.getLogger().setLevel(logging.CRITICAL)
     counter = 0
     title()
-    print(current_split_method)
-    return_split_method = print_ip_and_country()
+    print("\n\033[38;5;7mCurrent IP: {} : {} | Fetching Accounts\n".format(details[0], details[1]))
     user_options()
     options = input("Pick an option or (q)uit: ")
     while True:
         if options == "1":
             #Account Checker
             title()
-            if resume_flag == True:
+            if resume_flag:
                 print("\033[38;5;7m\nResume file found. Resuming from given combo.")
-            print_ip_and_country()
+            print("\n\033[38;5;7mCurrent IP: {} : {} | Fetching Accounts\n".format(details[0], details[1]))
             while counter < len(user):
                 if len(user) == 0:
                     print("\n\033[38;5;226mNo Accounts for current country.\n")
@@ -171,105 +101,33 @@ while True:
                     del updated_list[0]
                 if clear_page > 10:
                     title()
-                    print_ip_and_country()
+                    print("\n\033[38;5;7mCurrent IP: {} : {} | Fetching Accounts\n".format(details[0], details[1]))
                     clear_page = 0
                 time.sleep(1)
                 counter += 1
                 clear_page += 1
                 browser.close()
                 sys.stdout.write("\033[38;5;7m\x1b7\x1b[0;14fHits: %s Valid Accounts (Tried %s out of %s)\x1b8" % (
-                hits, str(counter), str(len(user))))
+                    hits, str(counter), str(len(user))))
                 sys.stdout.flush()
             print("\n\033[38;5;226mAll done.")
             input("\n\033[38;5;226mPress Enter.")
-            if resume_flag == True:
+            if resume_flag:
                 os.remove('resume')
             break
         if options == "2":
-            #Countries List in File and Stats
-            if return_split_method:
-                title()
-                combolist_countries = {}
-                key_words = ["Country: ", "Country = "]
-                counter = 0
-                lines_counter = 0
-                recurrence = 1
-                percentages = {}
-                combo_file = open('netflix', 'r')
-                combo_lines = combo_file.readlines()
-                full_countries_list_from_dict = list(countries)
-                while counter < len(full_countries_list_from_dict):
-                    while lines_counter < len(combo_lines):
-                        # here comes cancer
-                        if full_countries_list_from_dict[counter] == 'UK':
-                            special_countries = ['UK', 'GB']
-                            if [country for country in special_countries if (country in combo_lines[lines_counter])]:
-                                combolist_countries.update({full_countries_list_from_dict[counter]: recurrence})
-                                recurrence += 1
-                            lines_counter += 1
-                        if [key_word + countries[full_countries_list_from_dict[counter]] for key_word in key_words if (
-                                key_word + countries[full_countries_list_from_dict[counter]] in combo_lines[
-                            lines_counter])]:
-                            combolist_countries.update({full_countries_list_from_dict[counter]: recurrence})
-                            recurrence += 1
-                        lines_counter += 1
-                    recurrence = 1
-                    lines_counter = 0
-                    counter += 1
-                print("\033[38;5;7m\nCurrent Combo-list contains accounts created in the following countries:")
-                print("--------------------------------------------\n")
-                for country in combolist_countries:
-                    print("\033[38;5;220m{}: \033[38;5;190m{} Accounts in List".format(country.replace("_", " "),
-                                                                                       combolist_countries[country]))
-                    percentages.update(
-                        {country.replace("_", " "): round(combolist_countries[country] / len(combo_lines) * 100, 2)})
-                print("\033[38;5;7m\nMore Stats:")
-                print("--------------------------------------------\n")
-                print("\033[38;5;220mTotal Combos: \033[38;5;190m{}".format(len(combo_lines)))
-                if len(combolist_countries.values()) == 0:
-                    print("\033[38;5;220mNo Countries in list.")
-                    print("\nPress enter.")
-                    input()
-                    break
-                else:
-                    print("\033[38;5;220mMost Represented Country in List: \033[38;5;190m{}".format(
-                        list(combolist_countries.keys())
-                        [list(combolist_countries.values()).index(max(combolist_countries.values()))]))
-                    print("\033[38;5;220mCountries Percentages: ", end='')
-                    for value in percentages:
-                        print("\n\033[38;5;190m{}: {}%".format(value, percentages[value]), end='')
-                    input("\n\n\033[38;5;7mPress Enter to return")
-                    break
-            else:
-                title()
-                input("\033[38;5;7m\n\nNo countries to sort. \nPress Enter to return")
-                break
-
-        if options == "3":
-
-            whitelist = ['None', '[NETFLIX]', 'Short_line', ' |']
+            #Combo Splitter
             title()
-            try:
-                if [item for item in whitelist if (current_split_method == item)]:
-                    user = []
-                    passw = []
-                    with open('netflix', 'r') as net:
-                        for line in net.readlines():
-                            user.append(line.split(":")[0])
-                            passw.append(line.split(":")[1].split(" ")[0])
-                    print(current_split_method)
-                    current_split_method = "None"
-                    print(
-                        "\n\033[38;5;7mCountries in combos were deleted.\nYou can now check accounts without any specific VPN.")
-                else:
-                    print("\n\033[38;5;7mThere are no countries included in given combo-list")
-                list_without_countries = True
-                input("\nPress Enter to Return.")
-            except IndexError:
-                return_error()
-
+            user = []
+            passw = []
+            with open('netflix', 'r') as net:
+                for line in net.readlines():
+                    user.append(line.split(":")[0])
+                    passw.append(line.split(":")[1].split(" ")[0])
+            current_split_method = "None"
+            print("\n\033[38;5;7mUser : password combinations sorted.\n\nYou can now run the checker.")
+            input("\n\nPress Enter...")
         if options == "q":
-
             #Exit
             exit()
         else:
